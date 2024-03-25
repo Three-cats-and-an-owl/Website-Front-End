f4b4434b23b2e84159367917b32bcaf3efe686ff
LevelAndre <andrelee089@gmail.com>
2024-03-25 16:11:15 -0700

    react-create-app
    Initialize react app and add base components.

---
+++ beauty/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from 'react-bootstrap/Navbar'
+const Header = () => {
+  return (
+    <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">Beauty</Navbar.Brand>
+    </Navbar>
+  )
+}
+
+export default Header
---
+++ beauty/src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+import Header from '../components/Header'
+
+const Home = () => {
+  return (
+    <Header/>
+  )
+}
+
+export default Home
