b1ff4ff4b74b7daeb0af2cd871d918df518159a8
Jose Lee <andrelee089@gmail.com>
2024-03-27 16:39:14 -0700

    page

--- beauty/src/components/Header.js
+++ beauty/src/components/Header.js
@@ -5,19 +5,19 @@ import Button from 'react-bootstrap/Button'
 
 const Header = () => {
   return (
-    <Navbar expand="lg" style={{ backgroundColor: '#FFFAFA', height: '85px'}}>
+    <Navbar expand="lg" style={{ backgroundColor: '#FFFAFA', height: '85px' }}>
       <div className="d-flex justify-content-around w-50 mx-auto">
-        <Nav.Link href="#link1" className="text-center" style={{ fontFamily: 'Baskerville'}}>
+        <Nav.Link href="#link1" className="text-center" style={{ fontFamily: 'Baskerville' }}>
           <Button variant="outline-tertiary">Shop</Button>
         </Nav.Link>
-        <Nav.Link href="#link2" className="text-center" style={{ fontFamily: 'Baskerville'}}>
+        <Nav.Link href="#link2" className="text-center" style={{ fontFamily: 'Baskerville' }}>
           <Button variant="outline-tertiary">About</Button>
         </Nav.Link>
-        <Navbar.Brand href="#home" className="text-center" >Skincare Products</Navbar.Brand>
-        <Nav.Link href="#link3" className="text-center" style={{ fontFamily: 'Baskerville'}}>
+        <Navbar.Brand href="#home" className="text-center">Skincare Products</Navbar.Brand>
+        <Nav.Link href="#link3" className="text-center" style={{ fontFamily: 'Baskerville' }}>
           <Button variant="outline-tertiary">Cart</Button>
         </Nav.Link>
-        <Nav.Link href="#link4" className="text-center" style={{ fontFamily: 'Baskerville'}}>
+        <Nav.Link href="#link4" className="text-center" style={{ fontFamily: 'Baskerville' }}>
           <Button variant="outline-tertiary">Account</Button>
         </Nav.Link>
       </div>
--- beauty/src/pages/Home.js
+++ beauty/src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
 import Header from '../components/Header'
+import pic from '../assets/pictures/stock-homepage.jpg'
 
 const Home = () => {
   return (
@@ -9,7 +10,9 @@ const Home = () => {
       <Container>
         <Row>
           <Col>
-            
+          <div className="picture" style={{ padding: '10px', marginTop: '20px' }}>
+            <img src={pic} alt="stock-homepage" style={{ borderRadius: '10px', width: '100%', height: 'auto' }} />
+            </div>
           </Col>
         </Row>
       </Container>
