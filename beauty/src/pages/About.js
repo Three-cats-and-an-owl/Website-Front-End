57e39de2442abbc6ab22e98a892ce5d0f8f76816
alana-murry <89994505+alana-murry@users.noreply.github.com>
2024-04-10 17:37:32 -0700

    mine

--- beauty/src/pages/About.js
+++ beauty/src/pages/About.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 <!DOCTYPE html>
 <html lang="eng">
     <head>
@@ -59,3 +60,23 @@ document.getElementById('aboutContent').innerHTML = generateAboutContent(aboutIn
             </script>
         </body>
         </html>
+=======
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className="wrapper">
+    <div className="body">
+        About Us
+    </div>
+    <p>
+    At A.A.B.B. Skin, we are dedicated to crafting skincare solutions that nourish, protect, and enhance your natural beauty, inside and out.
+Founded on the principles of innovation, integrity, and inclusivity, A.A.B.B. Skin is committed to delivering high-quality products that cater to diverse skin types and concerns. Our formulations are meticulously curated, blending cutting-edge scientific research with time-tested botanical ingredients to create effective, results-driven skincare solutions.
+We understand that skincare is not one-size-fits-all, which is why we offer a diverse range of products designed to address specific needs, whether it's hydration, anti-aging, acne control, or brightening. Transparency and sustainability are core values that guide everything we do. We believe in transparency not only in our ingredients but also in our processes, ensuring that our customers can trust the products they use on their skin. Additionally, we are committed to minimizing our environmental footprint by using eco-friendly packaging and sourcing responsibly sourced ingredients whenever possible.
+    </p>
+    </div>
+  )
+}
+
+export default About
+>>>>>>> Stashed changes
