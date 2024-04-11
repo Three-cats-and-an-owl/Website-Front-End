5119fd941d9d2fcbdabf24b850b2bbe7b36df4c6
Beth Ann <124757398+bethann-hoot@users.noreply.github.com>
2024-04-10 18:01:59 -0700

    Update Account Login.js

--- beauty/src/pages/Account Login.js
+++ beauty/src/pages/Account Login.js
@@ -8,19 +8,18 @@ import './Account.css';
 const AccountLogin = () => {
     return (
         <Form>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Group className="login-container" controlId="formBasicEmail">
                 <Form.Label>Email address</Form.Label>
                 <Form.Control type="email" placeholder="Enter email"/>
             </Form.Group>
-
-            <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Group className="login-container" controlId="formBasicPassword">
                 <Form.Label>Password</Form.Label>
                 <Form.Control type="password" placeholder="Password"/>
             </Form.Group>
             <Button variant="primary" type="submit">
                 Submit
             </Button>
-            <Form.Text className="text-muted">
+            <Form.Text className="button">
                 Don't have an account? <Button variant="link">Create one here.</Button>
             </Form.Text>
         </Form>
@@ -28,27 +27,3 @@ const AccountLogin = () => {
 }
 export default AccountLogin;
 
-    // <div className="login-container">
-    // function LoginForm() {
-    //     return (
-    //         <Form>
-    //             <Form.Group className="mb-3" controlId="formBasicEmail">
-    //                 <Form.Label>Email address</Form.Label>
-    //                 <Form.Control type="email" placeholder="Enter email"/>
-    //             </Form.Group>
-    //
-    //             <Form.Group className="mb-3" controlId="formBasicPassword">
-    //                 <Form.Label>Password</Form.Label>
-    //                 <Form.Control type="password" placeholder="Password"/>
-    //             </Form.Group>
-    //             <Button variant="primary" type="submit">
-    //                 Submit
-    //             </Button>
-    //             <Form.Text className="text-muted">
-    //                 Don't have an account? <Button variant="link">Create one here.</Button>
-    //             </Form.Text>
-    //         </Form>
-    //
-    //
-    //     );
-    // }export default LoginForm;
