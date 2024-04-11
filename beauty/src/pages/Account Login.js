a3aabc995ad8ab6f58682e8467e8a0c8de77fed3
Beth Ann <124757398+bethann-hoot@users.noreply.github.com>
2024-04-10 17:45:19 -0700

    creating login js
    added form to account login js

--- beauty/src/pages/Account Login.js
+++ beauty/src/pages/Account Login.js
@@ -1,4 +1,54 @@
-import Header from '../components/Header'
-import Footer from '../components/Footer'
+// import Header from '../components/Header';
+// import Footer from '../components/Footer';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
-import
+import './Account.css';
+
+const AccountLogin = () => {
+    return (
+        <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email"/>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password"/>
+            </Form.Group>
+            <Button variant="primary" type="submit">
+                Submit
+            </Button>
+            <Form.Text className="text-muted">
+                Don't have an account? <Button variant="link">Create one here.</Button>
+            </Form.Text>
+        </Form>
+    );
+}
+export default AccountLogin;
+
+    // <div className="login-container">
+    // function LoginForm() {
+    //     return (
+    //         <Form>
+    //             <Form.Group className="mb-3" controlId="formBasicEmail">
+    //                 <Form.Label>Email address</Form.Label>
+    //                 <Form.Control type="email" placeholder="Enter email"/>
+    //             </Form.Group>
+    //
+    //             <Form.Group className="mb-3" controlId="formBasicPassword">
+    //                 <Form.Label>Password</Form.Label>
+    //                 <Form.Control type="password" placeholder="Password"/>
+    //             </Form.Group>
+    //             <Button variant="primary" type="submit">
+    //                 Submit
+    //             </Button>
+    //             <Form.Text className="text-muted">
+    //                 Don't have an account? <Button variant="link">Create one here.</Button>
+    //             </Form.Text>
+    //         </Form>
+    //
+    //
+    //     );
+    // }export default LoginForm;
--- beauty/src/pages/Account.js
+++ beauty/src/pages/Account.js
@@ -1,6 +1,8 @@
 import Header from '../components/Header'
 import { Container, Row, Col } from 'react-bootstrap'
 
+import AccountLogin from './Account Login'
+
 const Account = () => {
   return (
     <div className="Account">
@@ -8,6 +10,7 @@ const Account = () => {
         <Container>
             <Row>
                 Account Page
+                <AccountLogin/>
             </Row>
         </Container>
     </div>  )
