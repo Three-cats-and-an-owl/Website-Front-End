51835f91e0f6e94184d6db9075cc2249ac0d9310
Beth Ann <124757398+bethann-hoot@users.noreply.github.com>
2024-04-15 23:08:01 -0700

    4/15 update
    Converting html to javascript

--- beauty/src/pages/Account Creation.js
+++ beauty/src/pages/Account Creation.js
@@ -1,4 +1,65 @@
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
-import
+import './Account.css';
+import {Link} from "react-router-dom";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
+const AccountCreation = () => {
+        return (
+            <Form>
+                <Row className="login-container">
+                    <Form.Group  controlId="firstName">
+                        <Form.Label>First Name</Form.Label>
+                        <Form.Control type="firstName" placeholder=""/>
+                    </Form.Group>
+                    <Form.Group  controlId="lasName">
+                        <Form.Label>Last Name</Form.Label>
+                        <Form.Control type="lastName" placeholder=""/>
+                    </Form.Group>
+                    <Form.Group  controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder=""/>
+                    </Form.Group>
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder=""/>
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+
+                    <Form.Text className="button">
+
+                        Don't have an account?
+                        <Link to="/AccountCreation"><Button variant="link">Create one here.</Button></Link>
+                    </Form.Text>
+                </Row>
+            </Form>
+            // <div className="split-page">
+            // <Container>
+            //     <Row>
+            //         <Col>
+            //             <div className="left-section">
+            //                 <h2>CREATE ACCOUNT</h2>
+            //                 <br>
+            //                     <Form.Text className="button">Already have an account?
+            //                         <Link to: "/AccountLogin"><Button variant="link">Login here.</Button></Link>
+            //             </div>
+            //         </Col>
+            //
+            //         <Col>2 of 2</Col>
+            //
+            //     </Row>
+            //
+            // </Container>
+            // </div>
+);
+
+}
+
+export default AccountCreation;
--- beauty/src/pages/Account Login.js
+++ beauty/src/pages/Account Login.js
@@ -1,27 +1,32 @@
-// import Header from '../components/Header';
-// import Footer from '../components/Footer';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { Link } from 'react-router-dom';
+import Row from 'react-bootstrap/Row';
 
 import './Account.css';
 
 const AccountLogin = () => {
     return (
         <Form>
-            <Form.Group className="login-container" controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email"/>
-            </Form.Group>
-            <Form.Group className="login-container" controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password"/>
-            </Form.Group>
-            <Button variant="primary" type="submit">
-                Submit
-            </Button>
-            <Form.Text className="button">
-                Don't have an account? <Button variant="link">Create one here.</Button>
-            </Form.Text>
+            <Row className="login-container">
+                <Form.Group  controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder=""/>
+                </Form.Group>
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder=""/>
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+
+                <Form.Text className="button">
+
+                    Don't have an account?
+                    <Link to="/AccountCreation"><Button variant="link">Create one here.</Button></Link>
+                </Form.Text>
+            </Row>
         </Form>
     );
 }
