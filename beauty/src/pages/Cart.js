f85c106e07559f4073e938f609e2a5a7171d06cc
Brianna Rangel <brianna.rangel.978@my.csun.edu>
2024-04-17 17:12:42 -0700

    start

--- beauty/src/pages/Cart.js
+++ beauty/src/pages/Cart.js
@@ -1,15 +1,17 @@
 import Header from '../components/Header'
-import { Container, Row, Col } from 'react-bootstrap' 
+//import { Container, Row, Col } from 'react-bootstrap' 
+import { Popover } from 'react-bootstrap' 
 
 const Cart = () => {
   return (
     <div className="Cart">
         <Header/>
-        <Container>
-            <Row>
-                Cart Page
-            </Row>
-        </Container>
+        <Popover
+            id="cart-tab"
+            trigger="click"
+            placement="right"
+            title="Cart">
+        </Popover>
     </div>
   )
 }
