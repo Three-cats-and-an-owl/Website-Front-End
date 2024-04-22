996bc6a88703ed98500413b002808a3cf405f9ea
Jose Lee <andrelee089@gmail.com>
2024-04-21 19:09:39 -0700

    Axios

--- beauty/src/pages/Inventory.js
+++ beauty/src/pages/Inventory.js
@@ -1,17 +1,34 @@
-import Header from '../components/Header'
-import { Container, Row, Col } from 'react-bootstrap'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Header from '../components/Header';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const Inventory = () => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios('http://localhost:8080/api/v1/products');
+      setItems(result.data);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="Inventory">
       <Header/>
       <Container>
-            <Row>
-                Inventory Page
-            </Row>
-        </Container>
+        {items.map((item, index) => (
+          <Row key={index}>
+            <Col>{item.product_name}</Col>
+            <Col>{item.product_type}</Col>
+            <Col>{item.price}</Col>
+          </Row>
+        ))}
+      </Container>
     </div>
-  )
-}
+  );
+};
 
-export default Inventory
+export default Inventory;
